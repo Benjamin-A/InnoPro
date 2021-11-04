c05a1fcf04f06897ef4a820c1e2990fe9ef91092
Benjamin-A <benjamin.roald.andersen@gmail.com>
2021-11-04 10:02:02 +0100

    Made sepparate q_pages for each guest and connected them to choose_guest

--- webkurs/elsysapp/urls.py
+++ webkurs/elsysapp/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path
-from .views import get_sensor_data, index, q_page,chart,see_results, choose_guest #Relativ import av viewsfunksjonen
+from .views import get_sensor_data, index, q_page0, q_page1, q_page2 ,chart,see_results, choose_guest #Relativ import av viewsfunksjonen
 
 appname = "elsysapp"
 urlpatterns = [
     path('', index, name='index'),
-    path('q_page', q_page, name='q_page'),
+    path('q_page0', q_page0, name='q_page0'),
+    path('q_page1', q_page1, name='q_page1'),
+    path('q_page2', q_page2, name='q_page2'),
     path('see_results', see_results, name='see_results'),
     path('choose_guest', choose_guest, name='choose_guest'),
     
